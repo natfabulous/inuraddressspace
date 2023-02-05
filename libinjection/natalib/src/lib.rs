e22fcc43d17b4b59e2686ca1db61a9e781e81246
Natalie Keenan <natalie@natfabulous.com>
2023-02-05 13:07:09 -0800

    spawn native message box

--- libinjection/natalib/src/lib.rs
+++ libinjection/natalib/src/lib.rs
@@ -1,6 +1,16 @@
+use windows::{
+    Win32::{ UI::WindowsAndMessaging::{ MessageBoxW, MB_OK }, Foundation::HWND },
+    core::PCWSTR,
+};
+use u16cstr::u16cstr;
 
 #[ctor::ctor]
-fn ctor() {
-    println!("Hello from Rust!");
+unsafe fn ctor() {
+    MessageBoxW(
+        HWND::default(),
+        PCWSTR(u16cstr!("I'm in ur address space, executing code!").as_ptr()),
+        PCWSTR(u16cstr!("A message from rust").as_ptr()),
+        MB_OK
+    );
     std::process::exit(112)
 }
--- libinjection/nataliedotexe/src/main.rs
+++ libinjection/nataliedotexe/src/main.rs
@@ -1,32 +1,34 @@
 use std::path::PathBuf;
 use std::io::stdin;
 use argh::FromArgs;
-use color_eyre::{eyre::eyre, Report, Result };
+use color_eyre::{ eyre::eyre, Report, Result };
 
-use widestring::{u16cstr, U16CString, U16String};
+use widestring::{ u16cstr, U16CString, U16String };
 use windows::{
-        core::{PCWSTR,PCSTR, PWSTR},
-        Win32::
-        {
-            System::
-            {
-                LibraryLoader::{ LoadLibraryW, GetModuleHandleW, GetProcAddress},
-                Diagnostics::Debug::{WriteProcessMemory, FORMAT_MESSAGE_FROM_SYSTEM, FORMAT_MESSAGE_IGNORE_INSERTS, FormatMessageW}, 
-                Memory::{
-                    MEM_COMMIT, 
-                    PAGE_EXECUTE_READWRITE, 
-                    VirtualAllocEx, MEM_RESERVE
-                }, 
-                Threading::{
-                    OpenProcess, 
-                    PROCESS_QUERY_INFORMATION, 
-                    PROCESS_CREATE_THREAD, 
-                    PROCESS_VM_OPERATION, 
-                    PROCESS_VM_READ, 
-                    PROCESS_VM_WRITE, 
-                    CreateRemoteThread
-            }}, 
-        Foundation::GetLastError}};
+    core::{ PCWSTR, PCSTR, PWSTR },
+    Win32::{
+        System::{
+            LibraryLoader::{ GetModuleHandleW, GetProcAddress },
+            Diagnostics::Debug::{
+                WriteProcessMemory,
+                FORMAT_MESSAGE_FROM_SYSTEM,
+                FORMAT_MESSAGE_IGNORE_INSERTS,
+                FormatMessageW,
+            },
+            Memory::{ MEM_COMMIT, PAGE_EXECUTE_READWRITE, VirtualAllocEx, MEM_RESERVE },
+            Threading::{
+                OpenProcess,
+                PROCESS_QUERY_INFORMATION,
+                PROCESS_CREATE_THREAD,
+                PROCESS_VM_OPERATION,
+                PROCESS_VM_READ,
+                PROCESS_VM_WRITE,
+                CreateRemoteThread,
+            },
+        },
+        Foundation::GetLastError,
+    },
+};
 
 // Init parse args
 /// This Doc Comment is NON-OPTIONAL!
@@ -40,20 +42,22 @@ struct Args {
 fn main() -> Result<(), Report> {
     color_eyre::install()?; // idk
     let args: Args = argh::from_env(); // actually read args
-    unsafe { do_crimes(args)?; } // all the code (unsafe, unsurprisingly)
+    unsafe {
+        do_crimes(args)?;
+    } // all the code (unsafe, unsurprisingly)
     Ok(())
 }
 
 unsafe fn get_last_error() -> Report {
     let error_code = GetLastError().0;
-    let mut buf = vec![0u16,16384];
-    let len =FormatMessageW(
-        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, 
-        Some(std::ptr::null()), 
-        error_code, 
-        0, 
-        PWSTR(buf.as_mut_ptr()), 
-        buf.len() as u32, 
+    let mut buf = vec![0u16, 16384];
+    let len = FormatMessageW(
+        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        Some(std::ptr::null()),
+        error_code,
+        0,
+        PWSTR(buf.as_mut_ptr()),
+        buf.len() as u32,
         Some(std::ptr::null_mut())
     ) as usize;
     buf.truncate(len);
@@ -67,25 +71,21 @@ unsafe fn do_crimes(args: Args) -> Result<(), Report> {
 
     let pid = args.pid;
 
-    let access = 
-        PROCESS_CREATE_THREAD 
-        | PROCESS_QUERY_INFORMATION 
-        | PROCESS_VM_OPERATION 
-        | PROCESS_VM_READ 
-        | PROCESS_VM_WRITE;
+    let access =
+        PROCESS_CREATE_THREAD |
+        PROCESS_QUERY_INFORMATION |
+        PROCESS_VM_OPERATION |
+        PROCESS_VM_READ |
+        PROCESS_VM_WRITE;
 
-    let proc = OpenProcess(
-        access, 
-        false, 
-        pid
-    )?;
+    let proc = OpenProcess(access, false, pid)?;
     println!("Opened Process {proc:?}");
 
     let target_addr = VirtualAllocEx(
         proc,
-        Some(std::ptr::null_mut()), 
-        1024, 
-        MEM_RESERVE | MEM_COMMIT, 
+        Some(std::ptr::null_mut()),
+        1024,
+        MEM_RESERVE | MEM_COMMIT,
         PAGE_EXECUTE_READWRITE
     );
     if target_addr.is_null() {
@@ -99,23 +99,20 @@ unsafe fn do_crimes(args: Args) -> Result<(), Report> {
     // stdin().read_line(&mut String::new())?;
     let mut written = 0;
     WriteProcessMemory(
-        proc, 
-        target_addr, 
-        dll_path.as_ptr() as _, 
-        (dll_path.len() + 1) * std::mem::size_of::<u16>(), 
-        Some(&mut written),
+        proc,
+        target_addr,
+        dll_path.as_ptr() as _,
+        (dll_path.len() + 1) * std::mem::size_of::<u16>(),
+        Some(&mut written)
     ).ok()?;
-    assert!(written>0);
-    
-    let kernel32 = GetModuleHandleW(
-        PCWSTR(u16cstr!("kernel32.dll").as_ptr()))?;
+    assert!(written > 0);
+
+    let kernel32 = GetModuleHandleW(PCWSTR(u16cstr!("kernel32.dll").as_ptr()))?;
     println!("kernel32 handle: {kernel32:?}");
 
-    let load_library_w = GetProcAddress(
-        kernel32, 
-        PCSTR("LoadLibraryW\0".as_ptr())
-    )
-    .ok_or(eyre!("LoadLibraryW not found!"))? as *const ();
+    let load_library_w = GetProcAddress(kernel32, PCSTR("LoadLibraryW\0".as_ptr())).ok_or(
+        eyre!("LoadLibraryW not found!")
+    )? as *const ();
     println!("LoadLibraryW address: {load_library_w:p}");
 
     println!("Press enter to wreak havoc...");
@@ -123,12 +120,12 @@ unsafe fn do_crimes(args: Args) -> Result<(), Report> {
 
     let mut tid: u32 = 0;
     let thread = CreateRemoteThread(
-        proc, 
-        Some(std::ptr::null()), 
-        0, 
-        Some(std::mem::transmute(load_library_w)), 
-        Some(target_addr), 
-        0, 
+        proc,
+        Some(std::ptr::null()),
+        0,
+        Some(std::mem::transmute(load_library_w)),
+        Some(target_addr),
+        0,
         Some(&mut tid)
     )?;
 
